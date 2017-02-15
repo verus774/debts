58b208c935f7faa4c40efaba7b25bc7d82c887de
Sergey Ladutko <verus774@mail.ru>
2017-02-16 00:04:42 +0300

    showSuccessSnakbar util method added

---
+++ app/src/main/java/by/verus/debts/Utils.java
@@ -0,0 +1,18 @@
+package by.verus.debts;
+
+
+import android.content.Context;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+
+public class Utils {
+
+    public static void showSuccessSnakbar(Context context, View rootView, String message) {
+        Snackbar snackbar = Snackbar.make(rootView, message, Snackbar.LENGTH_SHORT);
+        View snackBarView = snackbar.getView();
+        snackBarView.setBackgroundColor(ContextCompat.getColor(context, R.color.snackbar_success));
+        snackbar.show();
+    }
+
+}
