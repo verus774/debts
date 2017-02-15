a14d7d5a814afedf3f4b9b79c7dd3ffab66d9afb
Sergey Ladutko <verus774@mail.ru>
2017-02-15 22:59:51 +0300

    update debt list after deleting debt

--- app/src/main/java/by/verus/debts/DebtAdapter.java
+++ app/src/main/java/by/verus/debts/DebtAdapter.java
@@ -1,14 +1,19 @@
 package by.verus.debts;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
+import android.widget.TextView;
 
 import java.util.List;
 
-public class DebtAdapter extends RecyclerView.Adapter<DebtHolder> {
+public class DebtAdapter extends RecyclerView.Adapter<DebtAdapter.DebtHolder> {
     private List<Debt> mDebts;
 
 
@@ -33,4 +38,72 @@ public class DebtAdapter extends RecyclerView.Adapter<DebtHolder> {
     public int getItemCount() {
         return mDebts.size();
     }
+
+    public void deleteDebt(int position) {
+        mDebts.remove(position);
+        notifyItemRemoved(position);
+    }
+
+
+    public class DebtHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private Debt mDebt;
+        private TextView mNameTextView;
+        private TextView mSumTextView;
+        private TextView mDateTextView;
+        private TextView mMoreTextView;
+
+
+        public DebtHolder(View itemView) {
+            super(itemView);
+
+            mNameTextView = (TextView) itemView.findViewById(R.id.list_item_debt_name_text_view);
+            mSumTextView = (TextView) itemView.findViewById(R.id.list_item_debt_sum_text_view);
+            mDateTextView = (TextView) itemView.findViewById(R.id.list_item_debt_date_text_view);
+            mMoreTextView = (TextView) itemView.findViewById(R.id.list_item_debt_more_text_view);
+
+            itemView.setOnClickListener(this);
+        }
+
+        public void bindDebt(Debt debt) {
+            mDebt = debt;
+
+            mNameTextView.setText(mDebt.getName());
+            mSumTextView.setText(String.valueOf(mDebt.getSum()));
+            mDateTextView.setText(mDebt.getDate().toString());
+
+            mMoreTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(final View v) {
+                    final Context context = v.getContext();
+
+                    PopupMenu popup = new PopupMenu(context, v);
+                    popup.inflate(R.menu.menu_item_debt);
+
+                    popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            switch (item.getItemId()) {
+                                case R.id.menu_delete_debt:
+                                    DebtLab.deleteById(mDebt.getId());
+                                    deleteDebt(getAdapterPosition());
+                                    return true;
+                            }
+                            return false;
+                        }
+                    });
+
+                    popup.show();
+
+                }
+            });
+        }
+
+        @Override
+        public void onClick(View v) {
+            Context context = v.getContext();
+            Intent intent = DebtAddActivity.newIntent(context, mDebt.getId());
+            context.startActivity(intent);
+        }
+    }
+
 }
