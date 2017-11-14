132d29e76fbc1e454b4a2066657312b43d8b8110
Sergey Ladutko <verus774@mail.ru>
2017-11-14 20:51:23 +0300

    FragmentActivity added

--- app/src/main/java/by/verus/debts/ui/DebtAddActivity.java
+++ app/src/main/java/by/verus/debts/ui/DebtAddActivity.java
@@ -2,30 +2,13 @@ package by.verus.debts.ui;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 
-import by.verus.debts.R;
-
-public class DebtAddActivity extends AppCompatActivity {
+public class DebtAddActivity extends FragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if (fragment == null) {
-            fragment = DebtAddFragment.newInstance();
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-        }
-
+    protected Fragment createFragment() {
+        return DebtAddFragment.newInstance();
     }
 
     public static Intent newIntent(Context context) {
--- app/src/main/java/by/verus/debts/ui/DebtListActivity.java
+++ app/src/main/java/by/verus/debts/ui/DebtListActivity.java
@@ -1,29 +1,12 @@
 package by.verus.debts.ui;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 
-import by.verus.debts.R;
-
-public class DebtListActivity extends AppCompatActivity {
+public class DebtListActivity extends FragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if (fragment == null) {
-            fragment = new DebtListFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-        }
-
+    protected Fragment createFragment() {
+        return new DebtListFragment();
     }
 
 }
